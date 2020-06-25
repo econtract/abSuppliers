0551b3edbd6db1128d9c3d0e3802c84b7a10f813
Sjef van Doorn <sjef@letstalk.nl>
2020-06-25 13:16:14 +0200

    AANB-220 | Footer: fixes to function

--- AbSuppliers.php
+++ AbSuppliers.php
@@ -290,23 +290,27 @@ class AbSuppliers {
     public function displaySupplierPartnersForEnergy($atts)
     {
         $atts['partners_only'] = true;
+        $atts['cat']           = ['dualfuel_pack', 'electricity', 'gas'];
+        $atts['mod']           = !empty($atts['mod']) ? $atts['mod'] : 5;
+        $atts['mark-up']       = !empty($atts['mark-up']) ? $atts['mark-up'] : 'div';
+        $atts['mark-up-class'] = !empty($atts['mark-up-class']) ? $atts['mark-up-class'] : '';
 
         $atts = $this->processMultipleProductCats($atts);
 
         list($atts, $supplierLogos) = $this->preparedSuppliersLogoData($atts);
 
         $breakPoint = $atts['mod'];
-        $counter    = 0;
         $response   = '<div class="row">';
 
-        foreach ($supplierLogos as $supplier) {
+        foreach ($supplierLogos as $index => $supplier) {
             $atts['link'] = $this->generateProviderLink($atts, $supplier);
 
-            // If $counter is divisible by $breakpoint mod, create new row
-            if ($counter % $breakPoint == 0 && $counter != 0) {
+            // If index is divisible by $breakpoint mod, create new row
+            if ($index % $breakPoint === 0 && $index !== 0) {
                 // New div row
                 $response .= '</div><div class="row">';
             }
+
             $response .= '<' . $atts['mark-up'] .
                 ' class="' . $atts['mark-up-class'] . '">' .
                 '<a href="' . $atts['link'] . '"' .
@@ -315,7 +319,6 @@ class AbSuppliers {
                 ' alt="' . $supplier['name'] . '">' .
                 '</a>' .
                 '</' . $atts['mark-up'] . '>';
-            $counter++;
         }
 
         $response .= '</div>';
